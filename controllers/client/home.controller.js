d73c9fee4a596f9658f588990f1a3fdbf2c3d7aa
Nguyễn Thành Duy Tân <tanntd.2005@gmail.com>
2024-07-15 19:39:12 +0700

    route-controller-env-gitignore-cautrucFolderPug

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,3 +1,5 @@
 module.exports.index = (req, res) => {//Tên hàm controller ở đây là index là file chính của home
-    res.render('client/pages/home/index');//Đây là file controller dc navigate đến sau khi đúng route
+    res.render('client/pages/home/index', {
+        pageTitle: 'Trang chủ'
+    });//Đây là file controller dc navigate đến sau khi đúng route
 }
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,3 +1,5 @@
 module.exports.index =(req, res) => {//Tên hàm controller ở đây là index là file chính của product
-    res.render('client/pages/products/index.pug');//Đây là file controller dc navigate đến sau khi đúng route
+    res.render('client/pages/products/index.pug', {
+        pageTitle: "Trang danh sách sản phẩm" //Trả về view cho title=pageTitle ở file layouts/default.pug
+    });//Đây là file controller dc navigate đến sau khi đúng route
 }
