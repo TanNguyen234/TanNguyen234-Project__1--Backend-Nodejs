07df29941fde344ce8ebb56098a978b8c44ea1af
Nguyễn Thành Duy Tân <tanntd.2005@gmail.com>
2024-07-16 19:12:33 +0700

    Lam trang danh sach san pham

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,21 @@
-module.exports.index =(req, res) => {//Tên hàm controller ở đây là index là file chính của product
+const Product = require('../../models/product.model');
+
+module.exports.index = async (req, res) => {//Tên hàm controller ở đây là index là file chính của product
+
+    const products = await Product.find({
+        status: 'active',
+        deleted: false
+    });
+
+    const newProducts = products.map(item => {
+        item.priceNew = item.price - (item.price*item.discountPercentage/100).toFixed(1);
+        return item;
+    })
+
+    console.log(products); //In ra mảng sản phẩm để kiểm tra
+
     res.render('client/pages/products/index.pug', {
-        pageTitle: "Trang danh sách sản phẩm" //Trả về view cho title=pageTitle ở file layouts/default.pug
+        pageTitle: "Trang danh sách sản phẩm", //Trả về view cho title=pageTitle ở file layouts/default.pug
+        products: newProducts //Trả về mảng sản phẩm ở file views/client/pages/products/index.pug
     });//Đây là file controller dc navigate đến sau khi đúng route
 }
