fbf7e3f22a932ad0b7f05af38dcb97df0ea00984
TanNguyen234 <167159004+TanNguyen234@users.noreply.github.com>
2024-07-19 17:56:25 +0700

    Lay san pham in ra giao dien

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,15 @@
+const Product = require('../../models/product.model');
+
 // [GET] /admin/products
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+
+    const products = await Product.find({
+        deleted: false
+    })
+    console.log(products);
+
     res.render('admin/pages/product/index', {
-        pageTitle: 'Danh sản phẩm'
+        pageTitle: 'Danh sản phẩm',
+        products: products
     })
 }
