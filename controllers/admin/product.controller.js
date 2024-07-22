7c8c802db6e31cb7b458cd6aff0bf0c4a3c2e5c5
TanNguyen234 <167159004+TanNguyen234@users.noreply.github.com>
2024-07-22 20:11:40 +0700

    Toi uu phan trang

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,7 @@
 const Product = require('../../models/product.model');
 const filterStatusHelper = require('../../helpers/filterStatus');
 const searchHelper = require('../../helpers/search');
+const paginationHelper = require('../../helpers/pagination');
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
@@ -22,21 +23,12 @@ module.exports.index = async (req, res) => {
     }
 
     //Pagination
-    let objectPagination = {
+    const countProducts = await Product.countDocuments(find); // Hàm count trong mongoose để tổng số sản phẩm
+
+    let objectPagination = paginationHelper({
         currentPage: 1,
         limitItem: 4
-    }
-
-    if(req.query.page) {
-        objectPagination.currentPage = parseInt(req.query.page);
-    }
-
-    objectPagination.skip = ((objectPagination.currentPage - 1) * objectPagination.limitItem);
-
-    const countProducts = await Product.countDocuments(find); // Hàm count trong mongoose để tổng số sản phẩm
-    const totalPage = Math.ceil(countProducts/objectPagination.limitItem);
-    objectPagination.totalPage = totalPage;
-    //End Pagination
+    }, req.query, countProducts)
 
     const products = await Product.find(find).limit(objectPagination.limitItem).skip(objectPagination.skip);   //Dùng find(hàm của mongoose) để lọc các dữ liệu từ database
 
