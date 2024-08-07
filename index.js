cc2eefa145e4f0db53acc9888e9d9d3e37cd4054
TanNguyen234 <tanntd.2005@gmail.com>
2024-08-07 16:20:43 +0700

    TinyMCE

--- index.js
+++ index.js
@@ -1,47 +1,53 @@
-const express = require("express");//import express vì là file index chính của project
-const methodOverride = require('method-override');//Nhúng methodOverride để ghi đè
-const bodyParser = require('body-parser');
-const flash = require('express-flash');
-const cookieParser = require('cookie-parser');// thư viện này để parser khi server phản hồi về view
-const session = require('express-session'); 
-
-require('dotenv').config();
+const express = require("express"); //import express vì là file index chính của project
+const path = require("path"); //Thư viện có sẵn trong express js khai báo dùng cho tinymce
+const methodOverride = require("method-override"); //Nhúng methodOverride để ghi đè
+const bodyParser = require("body-parser");
+const flash = require("express-flash");
+const cookieParser = require("cookie-parser"); // thư viện này để parser khi server phản hồi về view
+const session = require("express-session");
+require("dotenv").config();
 
 const database = require("./config/database");
 
-const systemConfig = require('./config/system')
+const systemConfig = require("./config/system");
 
-database.connect(); 
+database.connect();
 
-const routeAdmin = require('./routes/admin/index.route.js')//import routes chính của admin
-const route = require("./routes/client/index.route.js");//import file route chính chứa tất cả các route
+const routeAdmin = require("./routes/admin/index.route.js"); //import routes chính của admin
+const route = require("./routes/client/index.route.js"); //import file route chính chứa tất cả các route
 const { getMaxListeners } = require("./models/product.model.js");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+//TinyMCE trình soạn thảo văn bản có file /tinymce/tinymce.min.js
+app.use(
+  "/tinymce",
+  express.static(path.join(__dirname, "node_modules", "tinymce"))
+);
+
 //parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended : false}));;
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(methodOverride("_method")); //Sử dụng library method override
 
-app.set("views", `${__dirname}/views`);//Thiết lập vào thắng folder views vì view là folder show ra website
-app.set("view engine", "pug");//Template engine có thể là PUG, EJS, Handlebars...
+app.set("views", `${__dirname}/views`); //Thiết lập vào thắng folder views vì view là folder show ra website
+app.set("view engine", "pug"); //Template engine có thể là PUG, EJS, Handlebars...
 
 //Flash thư viện cho thông báo cho express js
-app.use(cookieParser("YSUDGSGDJSGDJ"));//key ngâu nhiên cho tăng tính bảo mật
-app.use(session({ cookie: {maxAge : 60000} }));//Thời gian cookie tồn tại 60000 ml giây
+app.use(cookieParser("YSUDGSGDJSGDJ")); //key ngâu nhiên cho tăng tính bảo mật
+app.use(session({ cookie: { maxAge: 60000 } })); //Thời gian cookie tồn tại 60000 ml giây
 app.use(flash());
 
 // App Local Variables
-app.locals.prefixAdmin = systemConfig.prefixAdmin;//Tạo biến toàn cục dùng được ở mọi file [pug] để linh hoạt path
+app.locals.prefixAdmin = systemConfig.prefixAdmin; //Tạo biến toàn cục dùng được ở mọi file [pug] để linh hoạt path
 
 //_dirname là cấu trúc thư mục của project dùng được local và online
-app.use(express.static(`${__dirname}/public`));// Nhúng file tĩnh để để hiểu rằng folder public chứa file tĩnh để public ra bên ngoài
+app.use(express.static(`${__dirname}/public`)); // Nhúng file tĩnh để để hiểu rằng folder public chứa file tĩnh để public ra bên ngoài
 
 //Routes
-routeAdmin(app);//Khi định import rồi thì truyền app vào
-route(app);//sử dụng route truyền app vào để sử dụng ở file index.route.js
+routeAdmin(app); //Khi định import rồi thì truyền app vào
+route(app); //sử dụng route truyền app vào để sử dụng ở file index.route.js
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ public/admin/js/tinymce-config.js
@@ -0,0 +1,4 @@
+tinymce.init({
+    selector: 'textarea.textarea-mce',//Cho thẻ textarea có class=textarea-mce  
+    plugins: 'advlist link image lists'//Để thêm tính năng insert img
+});
