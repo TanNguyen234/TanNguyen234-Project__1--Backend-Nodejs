65a1afbb9631c60761da34252033da243fc860b7
TanNguyen234 <tanntd.2005@gmail.com>
2024-08-10 18:24:37 +0700

    Xay dung trang danh sach sp admin phan 2

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,41 +1,58 @@
-const ProductCategory = require('../../models/product-category.model.js');
+const ProductCategory = require("../../models/product-category.model.js");
 
-const systemConfig = require('../../config/system');
+const systemConfig = require("../../config/system");
+
+const createTreeHelper = require('../../helpers/createTree.js');
 
 // [GET] /amin/product-category
 module.exports.index = async (req, res) => {
-    
-    let find = {
-        deleted: false
-    }
+  let find = {
+    deleted: false,
+  };
 
-    const records = await ProductCategory.find(find)
+  const records = await ProductCategory.find(find);
 
-    res.render('admin/pages/product-category/index', {   //Truyền ngược các giá trị lại View
-        pageTitle: 'Danh mục sản phẩm',
-        records: records,
-    })
-}
+  const newRecords = createTreeHelper.tree(records);
+
+  res.render("admin/pages/product-category/index", {  
+    pageTitle: "Danh mục sản phẩm",
+    records: newRecords,
+  });
+};
 
 // [GET] /amin/product-category/create
 module.exports.create = async (req, res) => {
-    res.render('admin/pages/product-category/create', {   //Truyền ngược các giá trị lại View
-        pageTitle: 'Tạo danh mục sản phẩm',
-    })
-}
+  let find = {
+    deleted: false,
+  };
+
+  const records = await ProductCategory.find(find);
+
+  const newRecords = createTreeHelper.tree(records);
+
+  console.log(newRecords);
+
+  newRecords.forEach(element => {
+    console.log(element.children);
+  });
+
+  res.render("admin/pages/product-category/create", {
+    pageTitle: "Tạo danh mục sản phẩm",
+    records: newRecords,
+  });
+};
 
 // [POST] /amin/product-category/create
 module.exports.createPost = async (req, res) => {
-    
-    if(req.body.position == '') {
-        const count = await ProductCategory.countDocuments();
-        req.body.position = count + 1;
-    } else {
-        req.body.position = parseInt(req.body.position);
-    }
+  if (req.body.position == "") {
+    const count = await ProductCategory.countDocuments();
+    req.body.position = count + 1;
+  } else {
+    req.body.position = parseInt(req.body.position);
+  }
 
-    const record = new ProductCategory(req.body);
-    await record.save();
+  const record = new ProductCategory(req.body);
+  await record.save();
 
-    res.redirect(`${systemConfig.prefixAdmin}/product-category`)
-}
+  res.redirect(`${systemConfig.prefixAdmin}/product-category`);
+};
