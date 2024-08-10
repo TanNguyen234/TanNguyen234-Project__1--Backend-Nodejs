1f14ffc243003677948280a7997e9ed651f6bb24
TanNguyen234 <tanntd.2005@gmail.com>
2024-08-10 18:44:41 +0700

    Xay dung trang quan ly danh muc sp 2 va fix stt

--- helpers/createTree.js
+++ helpers/createTree.js
@@ -1,8 +1,11 @@
+let count = 0;//Biến toàn cục chỉ được reset khi reset server chứ load lại thì biến vẫn ko được reset
 const createTree = (arr, parentId = "") => {//Recusion
     const tree = [];
     arr.forEach((item) => {
       if (item.parent_id === parentId) {
+        count++;
         const newItem = item
+        newItem.index = count;
         const children = createTree(arr, item.id);
         if (children.length > 0) {
           newItem.children = children;
@@ -14,6 +17,7 @@ const createTree = (arr, parentId = "") => {//Recusion
 }
 
 module.exports.tree = (arr, parentId = "") => {//Recusion
+    count = 0;
     const tree = createTree(arr, parentId ="");
     return tree;
 }
