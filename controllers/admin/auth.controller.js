02d67b617a35ba8b4d882429f6d9ce48a80fc71e
TanNguyen234 <tanntd.2005@gmail.com>
2024-08-19 15:33:34 +0700

    Them middleware cho route private

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -4,9 +4,14 @@ const systemConfig = require("../../config/system");
 
 // [GET] /admin/auth/login
 module.exports.login = (req, res) => {
-  res.render("admin/pages/auth/login.pug", {
-    titlePage: "Trang đăng nhập",
-  });
+  //Nếu user sửa token sẽ gây ra lặp vô hạn cho máy khách => chặn tư động
+  if(req.cookies.token) {
+    res.redirect(`${systemConfig.prefixAdmin}/dashboard`)
+  } else {
+    res.render("admin/pages/auth/login.pug", {
+      titlePage: "Trang đăng nhập",
+    });
+  }
 };
 // [POST] /admin/auth/login
 module.exports.loginPost = async (req, res) => {
@@ -26,7 +31,9 @@ module.exports.loginPost = async (req, res) => {
     res.redirect("back");
   } else if (user) {
     req.flash("success", "Đã đăng nhập thành công");
+
     res.cookie("token", user.token); //Trả token về frontend luu vào máy
+    
     res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
   } else {
     req.flash("error", "Email hoặc mật khẩu không đúng");
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,19 +1,26 @@
-const dashboardRoutes = require('./dashboard.route');
-const productRoutes = require('./product.route');
-const productCategoryRoutes = require('./product-category.route.js');
-const rolesRoutes = require('./roles.route.js');
-const accountsRoutes = require('./accounts.route.js');
-const authRoutes = require('./auth.route.js');
+const dashboardRoutes = require("./dashboard.route");
+const productRoutes = require("./product.route");
+const productCategoryRoutes = require("./product-category.route.js");
+const rolesRoutes = require("./roles.route.js");
+const accountsRoutes = require("./accounts.route.js");
+const authRoutes = require("./auth.route.js");
 
-const systemConfig = require('../../config/system.js');
+const middleware = require("../../middlewares/admin/auth.middleware.js");
+
+const systemConfig = require("../../config/system.js");
 
 module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAdmin;
-  
-    app.use(PATH_ADMIN + `/dashboard`, dashboardRoutes);//Nếu đúng route là "/" thì sẽ dẫn đến hàm controller mà ở đây tên hàm là index ở file controller admin
-    app.use(PATH_ADMIN + `/products`, productRoutes);
-    app.use(PATH_ADMIN + `/product-category`, productCategoryRoutes)   
-    app.use(PATH_ADMIN + `/roles`, rolesRoutes)
-    app.use(PATH_ADMIN + `/accounts`, accountsRoutes)
-    app.use(PATH_ADMIN + `/auth`, authRoutes)
-}
+
+    app.use(PATH_ADMIN + `/dashboard`, middleware.requireAuth, dashboardRoutes); //Nếu đúng route là "/" thì sẽ dẫn đến hàm controller mà ở đây tên hàm là index ở file controller admin
+
+    app.use(PATH_ADMIN + `/products`, middleware.requireAuth, productRoutes);
+
+    app.use(PATH_ADMIN + `/product-category`, middleware.requireAuth, productCategoryRoutes);
+
+    app.use(PATH_ADMIN + `/roles`, middleware.requireAuth, rolesRoutes);
+
+    app.use(PATH_ADMIN + `/accounts`, middleware.requireAuth, accountsRoutes);
+
+    app.use(PATH_ADMIN + `/auth`, authRoutes);
+};
