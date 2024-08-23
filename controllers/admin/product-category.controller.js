e63be3e9cb01f66104a0dd4b51e83e6c753e46cc
TanNguyen234 <tanntd.2005@gmail.com>
2024-08-23 15:32:38 +0700

    Fix phân quyền bên backend (postman post patch delete lam them)

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -44,6 +44,15 @@ module.exports.create = async (req, res) => {
 
 // [POST] /amin/product-category/create
 module.exports.createPost = async (req, res) => {
+  // const permissions = res.locals.role.permissions;
+  // if(permissions.includes('products-category_create')) {
+  //   console.log('có quyền')
+  //   ...code
+  // } else {
+  //   res.send('403')
+  //   return;
+  // }
+
   if (req.body.position == "") {
     const count = await ProductCategory.countDocuments();
     req.body.position = count + 1;
