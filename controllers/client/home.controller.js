99b761c1f5135a5c3673fa91dfeaade7585a79de
TanNguyen234 <tanntd.2005@gmail.com>
2024-08-23 17:55:47 +0700

    Hien thi danh sach sp moi nhat

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -3,6 +3,7 @@ const productsHelper = require('../../helpers/products');
 
 //[GET] /
 module.exports.index = async (req, res) => {//Tên hàm controller ở đây là index là file chính của home
+    //Lấy ra sp nổi bật
     const productsFeatured = await Product.find({
         status: 'active',
         deleted: false,
@@ -10,9 +11,21 @@ module.exports.index = async (req, res) => {//Tên hàm controller ở đây là
     }).limit(6);
 
     const newProducts = productsHelper.newPriceProducts(productsFeatured)
+    //Hết lấy ra sp nổi bật
+
+    //Lấy ra sp mới nhất
+    const lastestProducts = await Product.find({
+        status: 'active',
+        deleted: false
+    }).sort({position: "desc"}).limit(6);
+
+    const newLastestProducts = productsHelper.newPriceProducts(lastestProducts)
+
+    //Hết lấy ra sp mới nhất
 
     res.render('client/pages/home/index', {
         pageTitle: 'Trang chủ',
         productsFeatured: newProducts,
+        newLastestProducts: newLastestProducts
     });//Đây là file controller dc navigate đến sau khi đúng route
 }
