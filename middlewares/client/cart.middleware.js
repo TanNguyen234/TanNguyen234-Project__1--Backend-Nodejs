08c667f00f6094e2d4b13080f560b9b4e11dd6ab
TanNguyen234 <tanntd.2005@gmail.com>
2024-08-26 21:14:39 +0700

    Thêm nut miniCart

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -11,7 +11,13 @@ module.exports.cartId = async (req, res, next) => {
        
        res.cookie('cartId', cart.id, { expires: new Date(Date.now() + exprisesCookie) })
     } else {
-       
+       const cart = await Cart.findOne({
+         _id: req.cookies.cartId
+       })
+
+       cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity, 0)
+
+       res.locals.miniCart = cart
     }
 
     next();
