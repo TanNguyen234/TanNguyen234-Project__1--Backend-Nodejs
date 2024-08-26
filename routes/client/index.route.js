a9f5e7b5d87c16511543fe8429e55ffc52f9531b
TanNguyen234 <tanntd.2005@gmail.com>
2024-08-26 18:37:13 +0700

    Trang ket qua tim kiem

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,6 +2,7 @@ const categoryMiddleWare = require('../../middlewares/client/categories.middlewa
 
 const productRoutes = require('./product.route')
 const homeRoutes = require('./home.route')
+const searchRoutes = require('./search.route')
 
 module.exports = (app) => {
     app.use(categoryMiddleWare.category) //Mọi route bên client đều có product categories nên viết như này khác với auth bên admin
@@ -9,4 +10,6 @@ module.exports = (app) => {
     app.use("/", homeRoutes);//Nếu đúng route thì sẽ dẫn đến controller và để tách nhỏ ra theo mô hình MVC thì phải import vào
 
     app.use("/products", productRoutes);//vd: homeRoutes và productRoutes
+
+    app.use("/search", searchRoutes);//vd: homeRoutes và productRoutes
 }
