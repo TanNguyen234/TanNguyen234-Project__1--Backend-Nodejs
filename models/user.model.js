5e3c90a3b0f17a171481a91f24d30e692f771464
TanNguyen234 <tanntd.2005@gmail.com>
2024-08-30 17:02:49 +0700

    Tạo collection users

---
+++ models/user.model.js
@@ -0,0 +1,28 @@
+//File thiết lập schema của product
+const mongoose = require("mongoose");
+const generate = require('../helpers/generate');
+
+const usersSchema = new mongoose.Schema({//Thiết lập schema
+  fullName: String,
+  email: String,
+  password: String,
+  tokenUser: {
+    type: String,
+    default: generate.generateRandomString(20)
+  },
+  phone: String,
+  avatar: String,
+  status: {
+    type: String,
+    default: "active"
+  },
+  deleted: {
+    type: Boolean,
+    default: false
+  },
+  deleteAt: Date
+}, {timestamps: true});//Hàm mongoose nếu giá trị là true thì nó sẽ tự động lưu lại [ngày tạo] và khi uppdate nó cg tự động lưu [ngày updata]
+
+const Users = mongoose.model("Users", usersSchema, "users"); //Kết nối tới collection có tên products
+
+module.exports = Users;
